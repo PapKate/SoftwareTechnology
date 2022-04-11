b8cf3cd84c2f1d9b1e8241c1a3885aff5d2659f6
Katerina Papadopoulou <katerinakipapadopoulou24@gmail.com>
2022-04-11 16:03:27 +0300

    Project solution initialization

---
+++ App/VirtualReceptionist/VirtualReceptionist/DataModels/BaseModel.cs
@@ -0,0 +1,39 @@
+﻿namespace VirtualReceptionist
+{
+    /// <summary>
+    /// The base model
+    /// </summary>
+    public class BaseModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The date the model was created
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// The date the model was last modified
+        /// </summary>
+        public DateTime DateModified { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BaseModel() : base()
+        {
+
+        }
+
+        #endregion
+    }
+}
