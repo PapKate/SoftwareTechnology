3210a4c321c9e7c4657eb54a17dc59d9d19d163f
kwnnaa <ntinarobola@gmail.com>
2022-05-03 21:23:29 +0300

    Project initialization

---
+++ App/VirtualReceptionist/VirtualReceptionist/DataModels/BaseModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace VirtualReceptionist
+{
+    /// <summary>
+    /// The base model
+    /// </summary>
+    public class BaseModel
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The date the model was created
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// The date the model was last modified
+        /// </summary>
+        public DateTime DateModified { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BaseModel() : base()
+        {
+
+        }
+
+        #endregion
+    }
+}
