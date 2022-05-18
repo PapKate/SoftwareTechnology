563d5f8b9c93bead9e4998594690615ceabe86d3
kwnnaa <ntinarobola@gmail.com>
2022-05-18 16:09:45 +0300

    reservationw-events-code
    Co-Authored-By: PapKate <63216304+PapKate@users.noreply.github.com>
    Co-Authored-By: mariatsav <74532138+mariatsav@users.noreply.github.com>
    Co-Authored-By: Triantafyllos Prappas <93077970+progLime@users.noreply.github.com>

---
+++ App/VirtualReceptionist/VirtualReceptionist/Models/Reviews/Review.cs
@@ -0,0 +1,31 @@
+﻿namespace VirtualReceptionist
+{
+    public class Review
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The number of stars 
+        /// </summary>
+        public uint NumberOfStars { get; set; }
+
+        /// <summary>
+        /// The name
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public Review() : base()
+        {
+             
+        }
+
+        #endregion
+    }
+}
---
+++ App/VirtualReceptionist/VirtualReceptionist/Models/Reviews/RoomReview.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace VirtualReceptionist
+{
+    public class RoomReview
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The comments
+        /// </summary>
+        public string Comments { get; set; }
+
+        /// <summary>
+        /// The room's reviews
+        /// </summary>
+        public IEnumerable<Review> Reviews { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public RoomReview() : base()
+        {
+
+        }
+
+        #endregion
+    }
+}
