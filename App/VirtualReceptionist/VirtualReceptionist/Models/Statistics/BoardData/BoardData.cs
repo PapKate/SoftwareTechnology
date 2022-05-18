87195e7d5b43bd32d35d39470d3cabdc1c78928c
mariatsav <maria.tsavea@yahoo.gr>
2022-05-18 16:08:26 +0300

    statistics
    Co-Authored-By: PapKate <63216304+PapKate@users.noreply.github.com>
    Co-Authored-By: kwnnaa <101602406+kwnnaa@users.noreply.github.com>
    Co-Authored-By: Triantafyllos Prappas <93077970+progLime@users.noreply.github.com>

---
+++ App/VirtualReceptionist/VirtualReceptionist/Models/Statistics/BoardData/BoardData.cs
@@ -0,0 +1,29 @@
+﻿namespace VirtualReceptionist
+{
+    /// <summary>
+    /// Represents a statistics board
+    /// </summary>
+    public abstract class BoardData
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The title
+        /// </summary>
+        public string Title { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BoardData() : base()
+        {
+
+        }
+
+        #endregion
+    }
+}
---
+++ App/VirtualReceptionist/VirtualReceptionist/Models/Statistics/BoardStatistics/EventBoardStatistic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace VirtualReceptionist
+{
+    /// <summary>
+    /// Represents an event board statistic
+    /// </summary>
+    public class EventBoardStatistic
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// The number of participants
+        /// </summary>
+        public uint Participants { get; set; }
+
+        /// <summary>
+        /// The date
+        /// </summary>
+        public DateTime Date { get; set; }    
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public EventBoardStatistic() : base()
+        {
+
+        }
+
+        #endregion
+    }
+}
