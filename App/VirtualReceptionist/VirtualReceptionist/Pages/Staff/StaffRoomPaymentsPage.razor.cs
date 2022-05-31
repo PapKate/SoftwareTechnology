af096c9328c0a7da3968931392796992193fbc62
kwnnaa <ntinarobola@gmail.com>
2022-05-31 13:55:31 +0300

    Use cases 8 & 9 code implementation
    Co-Authored-By: PapKate <63216304+PapKate@users.noreply.github.com>
    Co-Authored-By: mariatsav <74532138+mariatsav@users.noreply.github.com>
    Co-Authored-By: Triantafyllos Prappas <93077970+progLime@users.noreply.github.com>

---
+++ App/VirtualReceptionist/VirtualReceptionist/Pages/Staff/StaffRoomPaymentsPage.razor.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace VirtualReceptionist.Pages.Staff
+{
+    public partial class StaffRoomPaymentsPage
+    {
+        #region Private Members
+
+        private Pin mCustomerPin;
+
+        private RoomCheckIn mRoomCheckIn;
+        private double mPrice;
+        private double mGiven;
+        private PaymentType mPaymentMethodInputValue = PaymentType.Paypal;
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// The hotel's pin
+        /// </summary>
+        [Parameter]
+        public Pin HotelPin { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public StaffRoomPaymentsPage() : base()
+        {
+
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void GetCustomerRoomPaymentData()
+        {
+            var customer = CustomerUser.GetCustomer(HotelPin, mCustomerPin);
+            mRoomCheckIn = customer.RoomCheckIn;
+            if(customer.RoomCheckIn.IsPaid)
+            {
+                HelperMethods.ShowMessage(MessageType.Information, "No due payments", "The stay in this room is already paid. Thank you for choosing us from Sahara Resort!");
+                return;
+            }
+
+            var numberOfNights = (uint)(mRoomCheckIn.DateEnded - mRoomCheckIn.DateCreated).Days;
+
+            mPrice = HelperMethods.CalculateTotalPrice(mRoomCheckIn.Room.Price, numberOfNights); 
+            // Show payments form
+            // -> PaymentsFormOKButton_OnClick()
+        }
+
+        private void PaymentsFormOKButton_OnClick()
+        {
+            if (mPaymentMethodInputValue == PaymentType.Cash)
+            {
+                var change = HelperMethods.SubmitPaymentWithCash(mPrice, mGiven);
+                RoomCheckInPaymentConfirmed();
+                return;
+            }
+
+            HelperMethods.SubmitPaymentWithPOS();
+        }
+
+        /// <summary>
+        /// The third party payment procedure for the creation of an event check in
+        /// </summary>
+        private void GoToPOSPayment()
+        {
+            var result = HelperMethods.ThirdPartyPayment(mPaymentMethodInputValue, mPrice);
+
+            // If the transaction was NOT successful
+            if (!result)
+            {
+                // Show the error
+                HelperMethods.ShowMessage(MessageType.Error, "Unsuccessful Payment", "The transaction was not successful. Please try again.");
+                return;
+            }
+            RoomCheckInPaymentConfirmed();
+        }
+
+        private void RoomCheckInPaymentConfirmed()
+        {
+            mRoomCheckIn.IsPaid = true;
+            // Show the message
+            HelperMethods.ShowMessage(MessageType.Information, "Successful Payment", "Your transaction has been completed and your check in paid. Thank you!");
+
+            // Sends the text to the phone
+            HelperMethods.SendPhoneText(mRoomCheckIn.Customer.Phone, $"Your payment for your stay has been confirmed. The total amount of your transaction was {mPrice}€. Thank you from Sahara Resort!");
+        }
+
+        #endregion
+    }
+}
